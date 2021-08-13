5fd1eab036897f0f0df95cf4f419086add69143e
YistYU <80993084+YistYU@users.noreply.github.com>
2021-08-13 13:35:53 +0800

    s

--- train_test.py
+++ train_test.py
@@ -19,6 +19,7 @@ import torch.optim
 import torch.utils.data
 import torch.utils.data.distributed
 import torchvision.models as models
+import keras.backend as K
 
 import pcl.loader
 import pcl.builder
@@ -27,6 +28,7 @@ import pcl.builder
 from sklearn import manifold, datasets
 from sklearn.cluster import KMeans
 import torch.nn.functional as F
+
 
 import scanpy as sc
 import pandas as pd
@@ -350,25 +352,26 @@ def main_worker(args):
             #print("embeddings")
             #print(embeddings)
             #print("TSNE Processing")
-            tsne = manifold.TSNE(n_components=3, init='pca', random_state=0)
-            prob_ATAC = tsne.fit_transform(embeddings_ATAC)
-            prob_RNA = tsne.fit_transform(embeddings_RNA)
-            ci_list = []
-            prob_ATAC = torch.from_numpy(prob_ATAC).cuda(args.gpu)
-            prob_RNA = torch.from_numpy(prob_RNA).cuda(args.gpu)
-            ci_list.append(prob_ATAC)
-            ci_list.append(prob_RNA)
-            num_view = 2
-            for i in range(num_view):
-                ci_list[i] = torch.sigmoid(ci_list[i])
-            x = torch.reshape(torch.matmul(ci_list[0].unsqueeze(-1), ci_list[1].unsqueeze(1)),(-1,pow(3,2),1))
-            for i in range(2,num_view):
-                x = torch.reshape(torch.matmul(x, ci_list[i].unsqueeze(1)),(3,i+1),1)
-            vcdn_feat = torch.reshape(x, (-1,pow(3, num_view)))
-            c = vcdn_feat.cpu().detach().numpy()
-            c = torch.from_numpy(c)
-            prob = F.softmax(c, dim=1).data.cpu().numpy()
-            pd_labels = prob.argmax(1)
+            embeddings = K.concatenate(embeddings_ATAC, embeddings_RNA)
+            tsne = manifold.TSNE(n_components=2, init='pca', random_state=0)
+            feat = tsne.fit_transform(embeddings)
+            # prob_ATAC = tsne.fit_transform(embeddings_ATAC)
+            # prob_RNA = tsne.fit_transform(embeddings_RNA)
+            # ci_list = []
+            # prob_ATAC = torch.from_numpy(prob_ATAC).cuda(args.gpu)
+            # prob_RNA = torch.from_numpy(prob_RNA).cuda(args.gpu)
+            # ci_list.append(prob_ATAC)
+            # ci_list.append(prob_RNA)
+            # num_view = 2
+            # for i in range(num_view):
+            #     ci_list[i] = torch.sigmoid(ci_list[i])
+            # x = torch.reshape(torch.matmul(ci_list[0].unsqueeze(-1), ci_list[1].unsqueeze(1)),(-1,pow(3,2),1))
+            # for i in range(2,num_view):
+            #     x = torch.reshape(torch.matmul(x, ci_list[i].unsqueeze(1)),(3,i+1),1)
+            # vcdn_feat = torch.reshape(x, (-1,pow(3, num_view)))
+            # c = vcdn_feat.cpu().detach().numpy()
+            # c = torch.from_numpy(c)
+            pd_labels = KMeans(n_clusters=3, random_state=seed).fit(feat).labels_
             #print(embeddings)
             #print("embeddings after tsne")
             # perform kmeans
